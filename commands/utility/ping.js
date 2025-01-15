d3a4f4dba93119b1c5133c0cb5910689f6ba3840
jacerea <jacerea@ad.unc.edu>
2025-01-15 00:49:56 -0500

    Folder of commands of first 3 basic '/' commands

---
+++ commands/utility/ping.js
@@ -0,0 +1,11 @@
+// This a simple ping pong type of commnand. I believe if you do /ping it replies with pong! idk ill check
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
